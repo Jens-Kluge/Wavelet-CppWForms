5b137fcfc6405216dd6700135fd6aac35de4c764
Jens-Kluge <jklu@arcor.de>
2021-02-14 22:49:42 +0100

    export UI updated

--- Wavelet CppWForms/frmExport.h
+++ Wavelet CppWForms/frmExport.h
@@ -38,6 +38,11 @@ namespace Wavelet_CppWForms {
 	private: System::Windows::Forms::Button^ btnClose;
 	private: System::Windows::Forms::Button^ btnExport;
 	private: System::Windows::Forms::SaveFileDialog^ sfdExport;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::RadioButton^ rb2DDWT;
+	private: System::Windows::Forms::RadioButton^ rb1DDWT;
+	private: System::Windows::Forms::RadioButton^ rbImage;
+	private: System::Windows::Forms::RadioButton^ rbSignal;
 	protected:
 
 	protected:
@@ -58,13 +63,19 @@ namespace Wavelet_CppWForms {
 			this->btnClose = (gcnew System::Windows::Forms::Button());
 			this->btnExport = (gcnew System::Windows::Forms::Button());
 			this->sfdExport = (gcnew System::Windows::Forms::SaveFileDialog());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->rbSignal = (gcnew System::Windows::Forms::RadioButton());
+			this->rbImage = (gcnew System::Windows::Forms::RadioButton());
+			this->rb1DDWT = (gcnew System::Windows::Forms::RadioButton());
+			this->rb2DDWT = (gcnew System::Windows::Forms::RadioButton());
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// btnClose
 			// 
-			this->btnClose->Location = System::Drawing::Point(68, 92);
+			this->btnClose->Location = System::Drawing::Point(197, 133);
 			this->btnClose->Name = L"btnClose";
-			this->btnClose->Size = System::Drawing::Size(116, 29);
+			this->btnClose->Size = System::Drawing::Size(139, 29);
 			this->btnClose->TabIndex = 0;
 			this->btnClose->Text = L"&Close";
 			this->btnClose->UseVisualStyleBackColor = true;
@@ -72,23 +83,84 @@ namespace Wavelet_CppWForms {
 			// 
 			// btnExport
 			// 
-			this->btnExport->Location = System::Drawing::Point(68, 38);
+			this->btnExport->Location = System::Drawing::Point(197, 79);
 			this->btnExport->Name = L"btnExport";
-			this->btnExport->Size = System::Drawing::Size(116, 31);
+			this->btnExport->Size = System::Drawing::Size(139, 31);
 			this->btnExport->TabIndex = 1;
-			this->btnExport->Text = L"E&xport to Excel";
+			this->btnExport->Text = L"E&xport to Excel file";
 			this->btnExport->UseVisualStyleBackColor = true;
 			this->btnExport->Click += gcnew System::EventHandler(this, &frmExport::btnExport_Click);
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->rb2DDWT);
+			this->groupBox1->Controls->Add(this->rb1DDWT);
+			this->groupBox1->Controls->Add(this->rbImage);
+			this->groupBox1->Controls->Add(this->rbSignal);
+			this->groupBox1->Location = System::Drawing::Point(26, 31);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(124, 137);
+			this->groupBox1->TabIndex = 2;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Dataset";
+			// 
+			// rbSignal
+			// 
+			this->rbSignal->AutoSize = true;
+			this->rbSignal->Location = System::Drawing::Point(8, 21);
+			this->rbSignal->Name = L"rbSignal";
+			this->rbSignal->Size = System::Drawing::Size(68, 21);
+			this->rbSignal->TabIndex = 0;
+			this->rbSignal->TabStop = true;
+			this->rbSignal->Text = L"Signal";
+			this->rbSignal->UseVisualStyleBackColor = true;
+			// 
+			// rbImage
+			// 
+			this->rbImage->AutoSize = true;
+			this->rbImage->Location = System::Drawing::Point(8, 75);
+			this->rbImage->Name = L"rbImage";
+			this->rbImage->Size = System::Drawing::Size(67, 21);
+			this->rbImage->TabIndex = 1;
+			this->rbImage->TabStop = true;
+			this->rbImage->Text = L"Image";
+			this->rbImage->UseVisualStyleBackColor = true;
+			// 
+			// rb1DDWT
+			// 
+			this->rb1DDWT->AutoSize = true;
+			this->rb1DDWT->Location = System::Drawing::Point(8, 48);
+			this->rb1DDWT->Name = L"rb1DDWT";
+			this->rb1DDWT->Size = System::Drawing::Size(83, 21);
+			this->rb1DDWT->TabIndex = 2;
+			this->rb1DDWT->TabStop = true;
+			this->rb1DDWT->Text = L"1D DWT";
+			this->rb1DDWT->UseVisualStyleBackColor = true;
+			// 
+			// rb2DDWT
+			// 
+			this->rb2DDWT->AutoSize = true;
+			this->rb2DDWT->Location = System::Drawing::Point(8, 102);
+			this->rb2DDWT->Name = L"rb2DDWT";
+			this->rb2DDWT->Size = System::Drawing::Size(83, 21);
+			this->rb2DDWT->TabIndex = 3;
+			this->rb2DDWT->TabStop = true;
+			this->rb2DDWT->Text = L"2D DWT";
+			this->rb2DDWT->UseVisualStyleBackColor = true;
 			// 
 			// frmExport
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(257, 150);
+			this->ClientSize = System::Drawing::Size(375, 210);
+			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->btnExport);
 			this->Controls->Add(this->btnClose);
 			this->Name = L"frmExport";
 			this->Text = L"frmExport";
+			this->Load += gcnew System::EventHandler(this, &frmExport::frmExport_Load);
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
@@ -121,5 +193,8 @@ namespace Wavelet_CppWForms {
 	private: System::Void btnClose_Click(System::Object^ sender, System::EventArgs^ e) {
 		Close();
 	}
-	};
+	private: System::Void frmExport_Load(System::Object^ sender, System::EventArgs^ e) {
+		rbSignal->Checked = true;
+	}
+};
 }
